b021a14f85bd6f8bcb859172243135af0319aa1d
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-06-17 12:15:01 +0000

    Add existing file

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  //write your code here 
+  document.getElementById("excuse").innerHTML = generateExcuse();
+  let generateExcuse = () => {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+  //To create a consistent excuse, you have to concatenate one item from each array in the proper order.
+    let subjectIndex = Math.floor(Math.random() * who.length); // random subject
+    // console.log(who[subjectIndex]); // result
+    let verbIndex = Math.floor(Math.random() * action.length); // random verb
+    // console.log(action[verbIndex]); // result
+    let objectIndex = Math.floor(Math.random() * what.length); // random object
+    // console.log(what[objectIndex]); // result
+    let clauseIndex = Math.floor(Math.random() * when.length); // random dependent clause
+    // console.log(when[clauseIndex]); // result
+
+  //Call the function onload and set the excuse into the innerHTML of the #excuse HTML element.
+  console.log(
+      document.getElementById("excuse").innerHTML =
+        who[subjectIndex] +
+        action[verbIndex] +
+        what[objectIndex] +
+        when[clauseIndex])
+  }
+}
